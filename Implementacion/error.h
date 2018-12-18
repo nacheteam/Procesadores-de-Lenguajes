d53efaf4377c2ef6762b8c17a0982647af7d2062
Pablo <pbaeyens31@gmail.com>
2018-12-18 19:42:27 +0100

    Arregla salida a fichero

--- Implementacion/error.h
+++ Implementacion/error.h
@@ -9,7 +9,7 @@ extern FILE* yyout;
 #define semprintf(f_, ...) {error++; linea_error(); fprintf(stderr, "Error semántico: "); fprintf(stderr, (f_), ##__VA_ARGS__); fflush(stderr); }
 
 
-#define genprintf(f_, ...) { if(!error){fprintf(yyout, (f_), ##__VA_ARGS__); fflush(stdout);} }
+#define genprintf(f_, ...) { if(!error){fprintf(yyout, (f_), ##__VA_ARGS__); fflush(yyout);} }
 
 void linea_error();
 void lerror(const char * msg);
