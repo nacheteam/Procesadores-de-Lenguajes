b86882a5bb6c94f9b7f5cbf7f01cb70f03ad6450
josemam <jmlosvillares@correo.ugr.es>
2018-12-19 02:24:19 +0100

    En C no hay referencias :(
    Muchos punteros.
    
    Además, uniformado el nombre del tipo a Lista, permitido avanzar y
    retroceder el cursor estando al borde (sin efecto; nuestra descripción
    dice que se puede) y algún par de detalles más.
    
    Ahora mismo es posible compilar un programa junto con `lista.c`

--- Implementacion/lista.c
+++ Implementacion/lista.c
@@ -1,9 +1,8 @@
-#include "listas.h"
+#include <stdlib.h> // exit
+#include "lista.h"
 
 //Si es 0 se desactiva el print, cualquier otro valor hace print.
 #define DBUG 1
-
-//TODO: generar la lista
 
 /******************************************************************************/
 /*                    Operadores sobre el cursor                              */
@@ -12,33 +11,24 @@
 /*
  * Aumenta el cursor si no te sales del tope de la lista
  */
-void avanzaCursor(Lista l){
-  if(l.cursor+1>=l.tope){
-    if(DBUG!=0)
-      printf("No se puede avanzar el cursor más allá del tope de la lista\n");
-    exit(1);
-  }
-  else{
-    l.cursor++;
-  }
+void avanzaCursor(Lista * l){
+  if(l->cursor+1 < l->tope)
+    l->cursor++;
 }
 
 /*
  * Decrementa el cursor si no te vas a los negativos
  */
-void retrocedeCursor(Lista l){
-  if(l.cursor-1<0 && DBUG!=0)
-    printf("No se puede retroceder el cursor a menos de 0\n");
-  else{
-    l.cursor--;
-  }
+void retrocedeCursor(Lista * l){
+  if(l->cursor > 0)
+    l->cursor--;
 }
 
 /*
  * Pone el cursor a 0
  */
-void reiniciaCursor(Lista l){
-  l.cursor=0;
+void reiniciaCursor(Lista * l){
+  l->cursor=0;
 }
 
 /******************************************************************************/
@@ -48,29 +38,29 @@ void reiniciaCursor(Lista l){
 /*
  * Devuelve el número de elementos de la lista que es el tope de la misma.
  */
-int numeroElementos(Lista l){
-  return(l.tope);
+int numeroElementos(Lista * l){
+  return(l->tope);
 }
 
 /*
  * Devuelve el elemento en la posición del cursor de la lista de enteros
  */
-int devuelveActualInt(Lista l){
-  return(lista_entero[cursor]);
+int devuelveActualInt(Lista * l){
+  return(l->lista_entero[l->cursor]);
 }
 
 /*
  * Devuelve el elemento en la posición del cursor de la lista de reales
  */
-double devuelveActualDouble(Lista l){
-  return(lista_real[cursor]);
+double devuelveActualDouble(Lista * l){
+  return(l->lista_real[l->cursor]);
 }
 
 /*
  * Devuelve el elemento en la posición del cursor de la lista de chars
  */
-char devuelveActualChar(Lista l){
-  return(lista_entero[cursor]);
+char devuelveActualChar(Lista * l){
+  return(l->lista_entero[l->cursor]);
 }
 
 /******************************************************************************/
@@ -80,9 +70,22 @@ char devuelveActualChar(Lista l){
 /*
  * Devuelve el elemento en la posición pos si la posición no se sale de los límites.
  */
-int devuelvePosicionInt(Lista l, int pos){
-  if(pos<l.tope && pos>=0)
-    return(l.lista_entero[pos]);
+int devuelvePosicionInt(Lista * l, int pos){
+  if(pos < l->tope && pos >= 0)
+    return(l->lista_entero[pos]);
+  else{
+    if(DBUG!=0)
+      printf("No se puede acceder a una posición de la lista fuera del rango\n"); // TODO: además de terminar la ejecución, sería razonable imprimir un error (un poco más detallado) en tiempo de ejecución siempre, eliminando el DBUG [aplíquese también a devuelvePosicionDouble y devuelvePosicionChar]
+    exit(1);
+  }
+}
+
+/*
+ * Devuelve el elemento en la posición pos si la posición no se sale de los límites.
+ */
+double devuelvePosicionDouble(Lista * l, int pos){
+  if(pos < l->tope && pos>=0)
+    return(l->lista_real[pos]);
   else{
     if(DBUG!=0)
       printf("No se puede acceder a una posición de la lista fuera del rango\n");
@@ -93,22 +96,9 @@ int devuelvePosicionInt(Lista l, int pos){
 /*
  * Devuelve el elemento en la posición pos si la posición no se sale de los límites.
  */
-double devuelvePosicionDouble(Lista l, int pos){
-  if(pos<l.tope && pos>=0)
-    return(l.lista_real[pos]);
-  else{
-    if(DBUG!=0)
-      printf("No se puede acceder a una posición de la lista fuera del rango\n");
-    exit(1);
-  }
-}
-
-/*
- * Devuelve el elemento en la posición pos si la posición no se sale de los límites.
- */
-char devuelvePosicionChar(Lista l, int pos){
-  if(pos<l.tope && pos>=0)
-    return(l.lista_char[pos]);
+char devuelvePosicionChar(Lista * l, int pos){
+  if(pos < l->tope && pos>=0)
+    return(l->lista_char[pos]);
   else{
     if(DBUG!=0)
       printf("No se puede acceder a una posición de la lista fuera del rango\n");
@@ -119,41 +109,41 @@ char devuelvePosicionChar(Lista l, int pos){
 /*
  * Devuelve un elemento de tipo lista con el elemento en la posición pos quitado
  */
-Lista eliminaElemento(Lista l, int pos){
+Lista eliminaElemento(Lista * l, int pos){
   Lista res;
   int contador = 0;
   //Ajustamos los valores del tope, cursor y tipo.
-  res.tope = l.tope-1;
-  if(l.cursor==tope-1)
-    res.cursor = l.cursor-1;
+  res.tope = l->tope-1;
+  if(l->cursor == l->tope-1)
+    res.cursor = l->cursor-1;
   else
-    res.cursor = l.cursor;
-  res.tipo = l.tipo;
+    res.cursor = l->cursor;
+  res.tipo = l->tipo;
 
   //En función del tipo rellenamos una lista u otra.
   if(res.tipo==listaentero || res.tipo==listabool){
     res.lista_entero = (int *) malloc(res.tope * sizeof(int));
-    for(int i = 0; i < l.tope; ++i){
+    for(int i = 0; i < l->tope; ++i){
       if(i!=pos){
-        res.lista_entero[contador] = l.lista_entero[i];
+        res.lista_entero[contador] = l->lista_entero[i];
         contador++;
       }
     }
   }
   else if(res.tipo==listareal){
     res.lista_real = (double *) malloc(res.tope * sizeof(double));
-    for(int i = 0; i < l.tope; ++i){
+    for(int i = 0; i < l->tope; ++i){
       if(i!=pos){
-        res.lista_entero[contador] = l.lista_entero[i];
+        res.lista_entero[contador] = l->lista_entero[i];
         contador++;
       }
     }
   }
   else if(res.tipo==listachar){
     res.lista_char = (char *) malloc(res.tope * sizeof(char));
-    for(int i = 0; i < l.tope; ++i){
+    for(int i = 0; i < l->tope; ++i){
       if(i!=pos){
-        res.lista_entero[contador] = l.lista_entero[i];
+        res.lista_entero[contador] = l->lista_entero[i];
         contador++;
       }
     }
@@ -164,33 +154,33 @@ Lista eliminaElemento(Lista l, int pos){
 /*
  * Devuelve una lista quitando los elementos a partir de la posición pos.
  */
-Lista eliminaFinal(Lista l, int pos){
+Lista eliminaFinal(Lista * l, int pos){
   Lista res;
   //Ajustamos los valores del tope, cursor y tipo.
   res.tope = pos+1;
-  if(l.cursor>=pos+1)
+  if(l->cursor>=pos+1)
     res.cursor = pos;
   else
-    res.cursor = l.cursor;
-  res.tipo = l.tipo;
+    res.cursor = l->cursor;
+  res.tipo = l->tipo;
 
   //En función del tipo rellenamos una lista u otra.
   if(res.tipo==listaentero || res.tipo==listabool){
     res.lista_entero = (int *) malloc(res.tope * sizeof(int));
-    for(int i = 0; i < l.tope && i<=pos; ++i){
-      res.lista_entero[i] = l.lista_entero[i];
+    for(int i = 0; i < l->tope && i<=pos; ++i){
+      res.lista_entero[i] = l->lista_entero[i];
     }
   }
   else if(res.tipo==listareal){
     res.lista_real = (double *) malloc(res.tope * sizeof(double));
-    for(int i = 0; i < l.tope && i<=pos; ++i){
-      res.lista_entero[o] = l.lista_entero[i];
+    for(int i = 0; i < l->tope && i<=pos; ++i){
+      res.lista_entero[i] = l->lista_entero[i];
     }
   }
   else if(res.tipo==listachar){
     res.lista_char = (char *) malloc(res.tope * sizeof(char));
-    for(int i = 0; i < l.tope && i<=pos; ++i){
-      res.lista_entero[o] = l.lista_entero[i];
+    for(int i = 0; i < l->tope && i<=pos; ++i){
+      res.lista_entero[i] = l->lista_entero[i];
     }
   }
   return(res);
@@ -199,45 +189,45 @@ Lista eliminaFinal(Lista l, int pos){
 /*
  * Concatena dos listas y devuelve una copia
  */
-Lista concatenaListas(Lista l1, Lista l2){
+Lista concatenaListas(Lista * l1, Lista * l2){
   Lista res;
   int contador = 0;
   //Actualiza tope, cursor y tipo de forma adecuada
-  res.tope = l1.tope+l2.tope;
-  res.cursor = 0;      //Lo pongo a 0 por ponerlo a algo
-  res.tipo = l1.tipo;
+  res.tope = l1->tope+l2->tope;
+  res.cursor = 0;
+  res.tipo = l1->tipo;
 
   //En función del tipo de la lista la generas en orden
   if(res.tipo==listaentero || res.tipo==listabool){
     res.lista_entero = (int *) malloc(res.tope * sizeof(int));
-    for(int i = 0; i < l1.tope; ++i){
-      res.lista_entero[contador] = l1.lista_entero[i];
+    for(int i = 0; i < l1->tope; ++i){
+      res.lista_entero[contador] = l1->lista_entero[i];
       contador++;
     }
-    for(int i = 0; i < l2.tope; ++i){
-      res.lista_entero[contador] = l2.lista_entero[i];
+    for(int i = 0; i < l2->tope; ++i){
+      res.lista_entero[contador] = l2->lista_entero[i];
       contador++;
     }
   }
   else if(res.tipo==listareal){
     res.lista_real = (double *) malloc(res.tope * sizeof(double));
-    for(int i = 0; i < l1.tope; ++i){
-      res.lista_real[contador] = l1.lista_real[i];
+    for(int i = 0; i < l1->tope; ++i){
+      res.lista_real[contador] = l1->lista_real[i];
       contador++;
     }
-    for(int i = 0; i < l2.tope; ++i){
-      res.lista_real[contador] = l2.lista_real[i];
+    for(int i = 0; i < l2->tope; ++i){
+      res.lista_real[contador] = l2->lista_real[i];
       contador++;
     }
   }
   else if(res.tipo==listachar){
     res.lista_char = (char *) malloc(res.tope * sizeof(char));
-    for(int i = 0; i < l1.tope; ++i){
-      res.lista_char[contador] = l1.lista_char[i];
+    for(int i = 0; i < l1->tope; ++i){
+      res.lista_char[contador] = l1->lista_char[i];
       contador++;
     }
-    for(int i = 0; i < l2.tope; ++i){
-      res.lista_char[contador] = l2.lista_char[i];
+    for(int i = 0; i < l2->tope; ++i){
+      res.lista_char[contador] = l2->lista_char[i];
       contador++;
     }
   }
@@ -247,57 +237,57 @@ Lista concatenaListas(Lista l1, Lista l2){
 /*
  * Suma x a los elementos de la lista
  */
-void sumaValorInt(Lista& l, int x){
-  for(int i = 0; i < l.tope; ++i)
-    l.lista_entero[i]+=x;
+void sumaValorInt(Lista * l, int x){
+  for(int i = 0; i < l->tope; ++i)
+    l->lista_entero[i]+=x;
 }
 
 /*
  * Suma x a los elementos de la lista
  */
-void sumaValorDouble(Lista& l, double x){
-  for(int i = 0; i < l.tope; ++i)
-    l.lista_real[i]+=x;
+void sumaValorDouble(Lista * l, double x){
+  for(int i = 0; i < l->tope; ++i)
+    l->lista_real[i]+=x;
 }
 
 /*
  * Resta x a los elementos de la lista
  */
-void restaValorInt(Lista& l, int x){
-  for(int i = 0; i < l.tope; ++i)
-    l.lista_entero[i]-=x;
+void restaValorInt(Lista * l, int x){
+  for(int i = 0; i < l->tope; ++i)
+    l->lista_entero[i]-=x;
 }
 
 /*
  * Resta x a los elementos de la lista
  */
-void restaValorDouble(Lista& l, double x){
-  for(int i = 0; i < l.tope; ++i)
-    l.lista_real[i]-=x;
+void restaValorDouble(Lista * l, double x){
+  for(int i = 0; i < l->tope; ++i)
+    l->lista_real[i]-=x;
 }
 
 /*
  * Multiplica x a los elementos de la lista
  */
-void productoValorInt(Lista& l, int x){
-  for(int i = 0; i < l.tope; ++i)
-    l.lista_entero[i]*=x;
+void productoValorInt(Lista * l, int x){
+  for(int i = 0; i < l->tope; ++i)
+    l->lista_entero[i]*=x;
 }
 
 /*
  * Multiplica x a los elementos de la lista
  */
-void productoValorDouble(Lista& l, double x){
-  for(int i = 0; i < l.tope; ++i)
-    l.lista_real[i]*=x;
+void productoValorDouble(Lista * l, double x){
+  for(int i = 0; i < l->tope; ++i)
+    l->lista_real[i]*=x;
 }
 
 /*
  * Divide por x a los elementos de la lista
  */
-void divideValorDouble(Lista& l, double x){
-  for(int i = 0; i < l.tope; ++i)
-    l.lista_real[i]/=x;
+void divideValorDouble(Lista * l, double x){
+  for(int i = 0; i < l->tope; ++i)
+    l->lista_real[i]/=x;
 }
 
 /******************************************************************************/
@@ -307,19 +297,21 @@ void divideValorDouble(Lista& l, double x){
 /*
  * Añade el elemento elem en la posición pos devolviendo una copia
  */
-Lista anadeElementoInt(Lista l, int pos, int elem){
+Lista anadeElementoInt(Lista * l, int pos, int elem){
   Lista res;
   int contador = 0;
-  res.tope = l.tope+1;
-  res.cursor = l.cursor;
-  res.tipo = l.tipo;
+  res.tope = l->tope+1;
+  res.cursor = l->cursor;
+  res.tipo = l->tipo;
+  if (res.cursor <= pos)
+    res.cursor++; // El cursor estará en el mismo elemento en el que estaba en la lista original
 
   res.lista_entero = (int *) malloc(res.tope * sizeof(int));
   for(int i = 0; i < res.tope; ++i){
     if(i==pos)
       res.lista_entero[i] = elem;
     else{
-      res.lista_entero[i] = l.lista_entero[contador];
+      res.lista_entero[i] = l->lista_entero[contador];
       contador++;
     }
   }
@@ -329,19 +321,21 @@ Lista anadeElementoInt(Lista l, int pos, int elem){
 /*
  * Añade el elemento elem en la posición pos devolviendo una copia
  */
-Lista anadeElementoDouble(Lista l, int pos, double elem){
+Lista anadeElementoDouble(Lista * l, int pos, double elem){
   Lista res;
   int contador = 0;
-  res.tope = l.tope+1;
-  res.cursor = l.cursor;
-  res.tipo = l.tipo;
+  res.tope = l->tope+1;
+  res.cursor = l->cursor;
+  res.tipo = l->tipo;
+  if (res.cursor <= pos)
+    res.cursor++; // El cursor estará en el mismo elemento en el que estaba en la lista original
 
   res.lista_real = (double *) malloc(res.tope * sizeof(double));
   for(int i = 0; i < res.tope; ++i){
     if(i==pos)
       res.lista_real[i] = elem;
     else{
-      res.lista_real[i] = l.lista_real[contador];
+      res.lista_real[i] = l->lista_real[contador];
       contador++;
     }
   }
@@ -351,19 +345,21 @@ Lista anadeElementoDouble(Lista l, int pos, double elem){
 /*
  * Añade el elemento elem en la posición pos devolviendo una copia
  */
-Lista anadeElementoChar(Lista l, int pos, char elem){
+Lista anadeElementoChar(Lista * l, int pos, char elem){
   Lista res;
   int contador = 0;
-  res.tope = l.tope+1;
-  res.cursor = l.cursor;
-  res.tipo = l.tipo;
+  res.tope = l->tope+1;
+  res.cursor = l->cursor;
+  res.tipo = l->tipo;
+  if (res.cursor <= pos)
+    res.cursor++; // El cursor estará en el mismo elemento en el que estaba en la lista original
 
   res.lista_char = (char *) malloc(res.tope * sizeof(char));
   for(int i = 0; i < res.tope; ++i){
     if(i==pos)
       res.lista_char[i] = elem;
     else{
-      res.lista_char[i] = l.lista_char[contador];
+      res.lista_char[i] = l->lista_char[contador];
       contador++;
     }
   }
--- Implementacion/lista.h
+++ Implementacion/lista.h
@@ -1,51 +1,48 @@
-#ifndef __LISTAS_H__
-#define __LISTAS_H__
+#ifndef __LISTA_H__
+#define __LISTA_H__
 
 #include "tabla.h"
 #include <string.h>
 #include <stdio.h>
 
-typedef struct List{
+typedef struct Lista{
   int * lista_entero;
   double * lista_real;
   char * lista_char;
   TipoDato tipo;       // Tipo de la lista
   int tope;            // Siguiente posición disponible de la lista
   int cursor;          // Posición actual del cabezal
-} List;
-
-// Rellena los elementos de la lista en función del tipo.
-void declaraLista(char* elementos, TipoDato tipo);
+} Lista;
 
 //Operaciones sobre el cursor
-void avanzaCursor(Lista l);     //Operador l>>
-void retrocedeCursor(Lista l);  //Operador l<<
-void reiniciaCursor(Lista l);   //Operador $l
+void avanzaCursor(Lista * l);     //Operador l>>
+void retrocedeCursor(Lista * l);  //Operador l<<
+void reiniciaCursor(Lista * l);   //Operador $l
 
 //Operadores unarios sobre la lista
-int numeroElementos(Lista l);      //Operador #l
-int devuelveActualInt(Lista l);    //Operador ?l
-double devuelveActualDouble(Lista l);    //Operador ?l
-char devuelveActualChar(Lista l);    //Operador ?l
+int numeroElementos(Lista * l);      //Operador #l
+int devuelveActualInt(Lista * l);    //Operador ?l
+double devuelveActualDouble(Lista * l);    //Operador ?l
+char devuelveActualChar(Lista * l);    //Operador ?l
 
 //Operadores binarios sobre la lista
-int devuelvePosicionInt(Lista l,int pos);    //Operador l@x
-double devuelvePosicionDouble(Lista l,int pos);    //Operador l@x
-char devuelvePosicionChar(Lista l,int pos);    //Operador l@x
-Lista eliminaElemento(Lista l,int pos);       //Operador l--x
-Lista eliminaFinal(Lista l, int pos);         //Operador l%x
-Lista concatenaListas(Lista l1, Lista l2);    //Operador l1**l2
-void sumaValorInt(Lista& l, int x);            //Operador l+x ó x+l
-void sumaValorDouble(Lista& l, double x);            //Operador l+x ó x+l
-void restaValorInt(Lista& l, int x);            //Operador l-x
-void restaValorDouble(Lista& l, double x);            //Operador l-x
-void productoValorInt(Lista& l, int x);            //Operador l*x ó x*l
-void productoValorDouble(Lista& l, double x);            //Operador l*x ó x*l
-void divideValorDouble(Lista& l, double x);            //Operador l/x
+int devuelvePosicionInt(Lista * l,int pos);    //Operador l@x
+double devuelvePosicionDouble(Lista * l,int pos);    //Operador l@x
+char devuelvePosicionChar(Lista * l,int pos);    //Operador l@x
+Lista eliminaElemento(Lista * l,int pos);       //Operador l--x
+Lista eliminaFinal(Lista * l, int pos);         //Operador l%x
+Lista concatenaListas(Lista * l1, Lista * l2);    //Operador l1**l2
+void sumaValorInt(Lista * l, int x);            //Operador l+x ó x+l
+void sumaValorDouble(Lista * l, double x);            //Operador l+x ó x+l
+void restaValorInt(Lista * l, int x);            //Operador l-x
+void restaValorDouble(Lista * l, double x);            //Operador l-x
+void productoValorInt(Lista * l, int x);            //Operador l*x ó x*l
+void productoValorDouble(Lista * l, double x);            //Operador l*x ó x*l
+void divideValorDouble(Lista * l, double x);            //Operador l/x
 
 //Operadores ternarios sobre la lista
-Lista anadeElementoInt(Lista l, int pos, int elem);        //Operador l++x@z
-Lista anadeElementoDouble(Lista l, int pos, double elem);        //Operador l++x@z
-Lista anadeElementoChar(Lista l, int pos, char elem);        //Operador l++x@z
+Lista anadeElementoInt(Lista * l, int pos, int elem);        //Operador l++x@z
+Lista anadeElementoDouble(Lista * l, int pos, double elem);        //Operador l++x@z
+Lista anadeElementoChar(Lista * l, int pos, char elem);        //Operador l++x@z
 
 #endif
